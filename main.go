e12ea9df1e59ab5b745e1914d8c087252363f620
Josh Vanderwillik <josh@wilik.ca>
2018-04-19 22:56:36 -0400

    Code cleanup

--- main.go
+++ main.go
@@ -7,18 +7,14 @@ import (
   "github.com/renstrom/fuzzysearch/fuzzy"
 )
 
-func main() {
-  debug := os.Getenv("DEBUG") != ""
+var debug bool
+
+func fileMatches(pattern string) []string {
   dir, err := os.Getwd()
   dir = dir+"/"
   if err != nil {
     panic(err)
   }
-  if len(os.Args) < 2 {
-    fmt.Println("Usage: ff <pattern>")
-    os.Exit(1)
-  }
-  pattern := os.Args[1]
   if debug {
     fmt.Fprintln(os.Stderr, "searching "+pattern+" in "+dir)
   }
@@ -33,6 +29,33 @@ func main() {
       files = append(files, rel_path)
     }
   }
+  return files
+}
+
+func openFile(path string) {
+  editor := os.Getenv("EDITOR")
+  if editor == "" {
+    fmt.Fprintf(os.Stderr, "$EDITOR not configured")
+    fmt.Println(path)
+  }
+  args := []string{path}
+  env := os.Environ()
+  if debug {
+    fmt.Fprintf(os.Stderr, "running %s %v\n", editor, args)
+  }
+  if err := syscall.Exec(editor, args, env); err != nil {
+    fmt.Fprintln(os.Stderr, err)
+    os.Exit(1)
+  }
+}
+
+func main() {
+  debug = os.Getenv("DEBUG") != ""
+  if len(os.Args) < 2 {
+    fmt.Println("Usage: ff <pattern>")
+    os.Exit(1)
+  }
+  files := fileMatches(os.Args[1])
   if len(files) == 0 {
     fmt.Fprintln(os.Stderr, "no matches")
     os.Exit(1)
@@ -45,24 +68,5 @@ func main() {
     // TODO josh: allow picking which one eventually
     os.Exit(1)
   }
-  file := files[0]
-  if debug {
-    for i, file := range files {
-      fmt.Fprintf(os.Stderr, "%d: %s\n", i, file)
-    }
-  }
-  editor := os.Getenv("EDITOR")
-  if editor == "" {
-    fmt.Fprintf(os.Stderr, "$EDITOR not configured")
-    fmt.Println(file)
-  }
-  args := []string{file}
-  env := os.Environ()
-  if debug {
-    fmt.Fprintf(os.Stderr, "running %s %v\n", editor, args)
-  }
-  if err := syscall.Exec(editor, args, env); err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(1)
-  }
+  openFile(files[0])
 }
